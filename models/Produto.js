c220d1745da49ed48a469fc57aec0c2ac187d8ad
LeonardoPCavalcanti <leozinhopcavalcanti@hotmail.com>
2025-07-04 15:53:50 -0300

    fix(models): adiciona campos de FK explícitos no model Produto

--- models/Produto.js
+++ models/Produto.js
@@ -2,29 +2,32 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Produto = sequelize.define('Produto', {
-    nome: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    descricao: DataTypes.TEXT,
-    unidade: DataTypes.STRING,
-    validade: DataTypes.DATE,
-    quantidade: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0
-    },
-    categoriaId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    fornecedorId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    setorId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    }
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  descricao: DataTypes.TEXT,
+  unidade: DataTypes.STRING,
+  validade: DataTypes.DATE,
+  quantidade: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  CategoriaId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { model: 'Categorias', key: 'id' },
+  },
+  FornecedorId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { model: 'Fornecedors', key: 'id' }, // Sequelize pluraliza automaticamente
+  },
+  SetorId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { model: 'Setors', key: 'id' }, // idem acima
+  },
 });
 
 module.exports = Produto;
